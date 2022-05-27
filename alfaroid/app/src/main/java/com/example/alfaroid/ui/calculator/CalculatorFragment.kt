4fcc50c5e9d46e8dbe689ea11cd5fefb09af0cab
Siti Mawaddah <siti.wadd@mhs.unsyiah.ac.id>
2022-05-28 03:01:29 +0700

    navigation

--- alfaroid/app/src/main/java/com/example/alfaroid/ui/calculator/CalculatorFragment.kt
+++ alfaroid/app/src/main/java/com/example/alfaroid/ui/calculator/CalculatorFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.alfaroid.R
 import com.example.alfaroid.databinding.FragmentCalculatorBinding
 
 class CalculatorFragment : Fragment() {
@@ -32,6 +34,11 @@ class CalculatorFragment : Fragment() {
 //        calculatorViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        val next1: Button = binding.next1
+        next1.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.calculator2Fragment)
+        }
         return root
     }
 
